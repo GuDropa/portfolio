e6354b716f0a28f7c01f6343e7e7efb65e4d3137
Gustavo Dropa <gustavo.dropa@gmail.com>
2026-05-19 00:39:31 -0300

    feat: added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import { ThemeProvider } from '@/components/providers/ThemeProvider';
 import '@/app/globals.scss';
-
+import { GoogleAnalytics } from '@next/third-parties/google';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning data-scroll-behavior="smooth">
+      <GoogleAnalytics gaId="G-L7YTDL8RTV" />
       <body>
         <ThemeProvider>
           {children}
